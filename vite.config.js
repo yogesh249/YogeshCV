93d45dde28ec792da8bf99c0f8e909e9dafc2366
yogesh249 <yogesh249@gmail.com>
2025-02-08 18:37:54 +0530

    pushing ggithub pages changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /D:/Yogesh/test-demo/vite-project/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/YogeshCV/', // Replace <REPO_NAME> with your repository name
+});
